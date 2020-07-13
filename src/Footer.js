4b1d49e6e87603bd813e29bcacaf1e77f80cc2f9
Yurii <yurich3098@gmail.com>
2020-07-13 19:11:25 +0300

    social network itkam

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+      <div className="App">
+         <a href="">News feed</a>
+         <a href="">Home</a>
+         <a href="">Messages </a>
+      </div>
+    );
+}
+  
+export default Footer;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Profile = () => {
+    return(
+        <div className='content'>
+            <div>
+            <img src="https://specials-images.forbesimg.com/imageserve/5ed424a52259c9000769e94c/960x0.jpg?cropX1=120&cropX2=1267&cropY1=96&cropY2=668" alt=""/>
+            </div>
+            <div>ava+description</div>
+            <div>my post
+                <div>new post</div>
+                <div>
+                    <div>post 1</div>
+                    <div>post 2</div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
