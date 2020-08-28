dcd87fdecb32cb332345d11b81c7dd52e48e467c
Yurii <yurich3098@gmail.com>
2020-08-28 20:19:34 +0300

    add css modules for each component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,11 +3,21 @@ import React from 'react';
 const Navbar = () => {
     return(
         <nav className='nav'>
-            <div><a href="">Profile</a></div>
-            <div><a href="">Messages</a></div>
-            <div><a href="">News</a></div>
-            <div><a href="">Music</a></div>
-            <div><a href="">Settings</a></div>
+            <div>
+                <a href="">Profile</a>
+            </div>
+            <div>
+                <a href="">Messages</a>
+            </div>
+            <div>
+                <a href="">News</a>
+            </div>
+            <div>
+                <a href="">Music</a>
+            </div>
+            <div>
+                <a href="">Settings</a>
+            </div>
         </nav>
     );
 }
